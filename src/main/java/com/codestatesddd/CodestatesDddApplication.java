adba833e6d19ba459cfebace255e61861b4a039d
gomtak <tak6831@gmail.com>
2022-04-21 23:31:58 +0900

    Remove: Delete Help.md File

---
+++ src/main/java/com/codestatesddd/CodestatesDddApplication.java
@@ -0,0 +1,13 @@
+package com.codestatesddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodestatesDddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodestatesDddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codestatesddd/CodestatesDddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestatesddd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodestatesDddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
